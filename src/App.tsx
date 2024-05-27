a9a6328ceecc1d47826e27e645ad17dcc7476790
heyakhil <luvprogramming@gmail.com>
2024-05-27 21:25:22 +0530

    Addon rohit changes 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import classes from "./App.module.css";
-import { Container } from "@mantine/core";
+import { Container, Transition } from "@mantine/core";
 import Slider from "react-slick";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useState } from "react";
+
+import Elevate from '../public/Elevate-2.png';
+import Edit from '../public/Edit-2.png';
+import Draft from '../public/Draft-2.png';
 
 function App() {
+  const [mounted, setMounted] = useState(false);
+
   const settings = {
     dots: false,
     infinite: true,
@@ -80,12 +87,22 @@ function App() {
           </div>
         </Container>
       </div>
-
       <div className="bid-write">
         <Container>
           <div className="row">
             <div className="col-md-8">
-              <h2 className="text-left">A new way of bid writing</h2>
+              <Transition
+                mounted={mounted}
+                transition="slide-up"
+                duration={400}
+                timingFunction="ease"
+              >
+                {(styles) => (
+                  <h2 style={styles} className="text-left">
+                    A new way of bid writing
+                  </h2>
+                )}
+              </Transition>
               <hr />
               <p className="max-w-text">
                 Bid teams spend 80% of their time in the drafting stage with
@@ -122,6 +139,189 @@ function App() {
           </div>
         </Container>
       </div>
+      <div className="ingredients">
+        <div className="container">
+            <div className="row">
+                <h2>The 4 key ingredients of our custom language engines</h2>
+                <hr />
+
+                <div className="tabBox">
+                    <div className="row upperBtns">
+                        <div className="col-md-3">
+                            <button className="btn btn-danger">WORLD ENGINEERING</button>
+                        </div>
+                        <div className="col-md-3">
+                            <button className="btn btn-danger">LINGUISTIC ENGINEERING</button>
+                        </div>
+                        <div className="col-md-3">
+                            <button className="btn btn-danger">YOUR KNOWLEDGE LIBRARY</button>
+                        </div>
+                        <div className="col-md-3">
+                            <button className="btn btn-danger">THE BEST UX/UI</button>
+                        </div>
+                    </div>
+                    <div className="tab-content">
+                        <div className="max-content">
+                            <h2>WORLD  ENGINEERING</h2>
+                            <p>We employ the top AI-engineers in the world. We combine their intelligence with the insight and experience of dozens of the most successful bid and proposal writing professionals. Our team has invested over 50,000 hours into defining the elements of compelling competitive prose and overcoming the complexities, challenges, and nuances involved in building software that wins more work and saves more time.</p>
+                            <p>We have developed a unique approach for optimising language. Our tool effectively conveys ideas and arguments by tailoring proposals to match the linguistic patterns, preferences, and nuances of the target audience.</p>
+                            <button className="btn btn-info">Learn More</button>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div className="key-feature">
+    <div className="container">
+        <h2>
+            Our key features
+        </h2>
+        <hr />
+        <div className="row">
+            <div className="col-md-6">
+                <h2 className="big">Draft</h2>
+                <hr />
+                <h3 className="big">Reduce drafting time by 70%</h3>
+                <div className="innerbox">
+                    <h4>Creative Ideation</h4>
+                    <p>Automatically generate winning bid ideas for a competitive edge in seconds.</p>
+                </div>
+                <div className="innerbox">
+                    <h4>Creative Ideation</h4>
+                    <p>Automatically generate winning bid ideas for a competitive edge in seconds.</p>
+                </div>
+                <div className="innerbox">
+                    <h4>Creative Ideation</h4>
+                    <p>Automatically generate winning bid ideas for a competitive edge in seconds.</p>
+                </div>
+                <div className="innerbox">
+                    <h4>Creative Ideation</h4>
+                    <p>Automatically generate winning bid ideas for a competitive edge in seconds.</p>
+                </div>
+            </div>
+            <div className="col-md-6">
+                <img src={Draft} className="w-100" alt="Draft" />
+            </div>
+        </div>
+
+        <div className="row">
+            <div className="col-md-6">
+                <img src={Edit} className="w-100" alt="Draft" />
+            </div>
+            <div className="col-md-6">
+                <h2 className="big">Edit</h2>
+                <hr />
+                <h3 className="big">Reduce drafting time by 70%</h3>
+                <div className="innerbox">
+                    <h4>Creative Ideation</h4>
+                    <p>Automatically generate winning bid ideas for a competitive edge in seconds.</p>
+                </div>
+                <div className="innerbox">
+                    <h4>Creative Ideation</h4>
+                    <p>Automatically generate winning bid ideas for a competitive edge in seconds.</p>
+                </div>
+                <div className="innerbox">
+                    <h4>Creative Ideation</h4>
+                    <p>Automatically generate winning bid ideas for a competitive edge in seconds.</p>
+                </div>
+                <div className="innerbox">
+                    <h4>Creative Ideation</h4>
+                    <p>Automatically generate winning bid ideas for a competitive edge in seconds.</p>
+                </div>
+            </div>
+        </div>
+
+        <div className="row">
+            <div className="col-md-6">
+                <h2 className="big">Elevate</h2>
+                <hr />
+                <h3 className="big">Reduce drafting time by 70%</h3>
+                <div className="innerbox">
+                    <h4>Creative Ideation</h4>
+                    <p>Automatically generate winning bid ideas for a competitive edge in seconds.</p>
+                </div>
+                <div className="innerbox">
+                    <h4>Creative Ideation</h4>
+                    <p>Automatically generate winning bid ideas for a competitive edge in seconds.</p>
+                </div>
+                <div className="innerbox">
+                    <h4>Creative Ideation</h4>
+                    <p>Automatically generate winning bid ideas for a competitive edge in seconds.</p>
+                </div>
+                <div className="innerbox">
+                    <h4>Creative Ideation</h4>
+                    <p>Automatically generate winning bid ideas for a competitive edge in seconds.</p>
+                </div>
+            </div>
+            <div className="col-md-6">
+                <img src={Elevate} className="w-100" alt="Draft" />
+            </div>
+        </div>
+
+    </div>
+</div>
+
+<div className="tesimonial">
+    <div className="container">
+        <h2>Our customers</h2>
+        <hr />
+        <p className="max-text">Our Language Engines are making our clients’ bid-writing teams up to 85% more efficient, allowing them to get to draft in minutes rather than days and boosting their win rates by up to 241%. But don’t just take our word for it, see what our customers are saying.</p>
+        <div className="row">
+            
+        </div>
+    </div>
+</div>
+
+<div className="commitment-sec dark-sec">
+    <div className="container">
+        <h2>AutogenAI's commitment to trust</h2>
+        <hr />
+        <p className="max-text">Trust is at the core of our business and embedded in everything we do. Businesses around the globe rely on AutogenAI to write winning prose, increase the quantity and quality of the proposals they submit, and gain a competitive edge for their most sensitive and time-critical bids. AutogenAI maintains the secure environments they’ve come to trust. </p>
+        <button className="btn btn-info">Learn More</button>
+    </div>
+</div>
+
+<div className="featured">
+    <div className="container">
+        <h2>As featured in</h2>
+        <hr />
+        <div className="row">
+            <div className="box">
+                <img src="" alt="" />
+            </div>
+        </div>
+    </div>
+</div>
+
+<div className="formbox">
+    <div className="container">
+        <h6>INTERESTED?</h6>
+        <h2>BOOK YOUR DEMO TODAY</h2>
+    </div>
+</div>
+
+<div className="news">
+    <div className="container">
+        <h2>LATEST NEWS & INSIGHTS.</h2>
+        <hr />
+        <p>WRITTEN IN COLLABORATION WITH AUTOGENAI’S GENERAL LANGUAGE ENGINE 1</p>
+
+        <div className="row">
+            <div className="col-md-4">
+                <div className="box">
+                    <img src="images/the-times-logo-white-p4ze56iubvr4oz3chtk85wy1thv28udldclwb80xz4.png" alt="" />
+                    <small>26TH MAY, 2024</small>
+                    <hr />
+                    <button className="btn btn-info">View Article</button>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
     </>
   );
 }
